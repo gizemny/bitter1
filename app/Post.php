c87f83665705ff6e2f44873ece1c73cd5fa3655a
Gizem <gizemny@gmail.com>
2016-03-05 18:28:17 -0600

    added relationship to model

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+	/**
+     * Get the user that owns the post.
+     */    
+	public function user () {
+		return $this->belongsTo('\App\User');
+	}
+
+	/**
+     * Get the users that favorited the post.
+     */   
+    public function postUser () {
+		return $this->belongsToMany('\App\User');
+	}
 }
