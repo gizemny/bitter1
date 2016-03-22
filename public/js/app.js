4b35010e146e334e80643cf4eb3a1dc938e95981
Gizem <gizemny@gmail.com>
2016-03-21 21:19:54 -0500

    added events - lookin good

--- public/js/app.js
+++ public/js/app.js
@@ -16,12 +16,17 @@ var PostsCollection = Backbone.Collection.extend({
 	model: PostModel
 });
 
+
 //these are objects nested in {} so we use : and , 
 //because it is an Item View, this will have the post model associated with it 
 var PostItemView = Backbone.View.extend({
 	el: '<li></li>',
 
 	template: _.template('<h2><%= post.get("description") %></h2>'),
+
+	initiate: function() {
+		this.listenTo(this.model, 'sync', this.render);
+	},
 
 	render: function() {
 		this.$el.html(this.template({ post: this.model })); 
@@ -31,33 +36,39 @@ var PostItemView = Backbone.View.extend({
 //if its list view them it is a collection 
 var PostsListView = Backbone.View.extend({
 	el: '<ul></ul>',
+
 	template: undefined,
+
+	initialize: function() {
+		this.listenTo(this.collection, 'all', function(event) {
+			console.log(event);
+		});
+		this.listenTo(this.collection, 'sync update', this.render);
+	},
 
 	render: function() {
 		var that = this; 
+		//pass in the collection 
 		this.collection.each(function(postModel) {
+			// instantiate the itemView by looping through each and add pass in your model template
 			var postItemView = new PostItemView({ model: postModel });
 			postItemView.render();
 			// this is how you attach el of post item view into this el 
 			that.$el.append(postItemView.el);
-			$('#content').html(postsListView.el);
 		}); 
 	}
 });
 
-// var post = new PostModel({id: 1});
+// post collection has memorized the models
+var posts = new PostsCollection(); 
 
-// //trying to stop from renders before we get a title
-// //because it is asychronous so we want a success callback
-// post.fetch({
-// 	success: function() {
-// 		//instantiate new one
-// 		var postItemView = new PostItemView({ model: post });
-// 		//render
-// 		postItemView.render();
-// 		//stick it into the .el and stick that into the content div from our home template
-// 		$('#content').html(postItemView.el);
-// 	}
-// });
+// need success call back because asychronous
+posts.fetch();
+
+var postsListView = new PostsListView({ collection: posts });
+postsListView.render();
+
+$('#content').html(postsListView.el);
+console.log('view inserted!');
 
 
